0c1da64869036e17db0d5e9376f1c0ff614522c0
Zarhkoh <zarhkoh@hotmail.fr>
2020-11-13 16:08:34 +0100

    ajout des routes News et Livraison

--- src/modules/router.modules.js
+++ src/modules/router.modules.js
@@ -2,6 +2,8 @@ const visitor = require('../routes/visitor.route');
 const timbre = require('../routes/timbre.route');
 const auth = require('../routes/auth.route');
 const devis = require('../routes/devis.route');
+const news = require('../routes/news.route');
+const livraison = require('../routes/livraison.route');
 const CONFIG = require('../config/config');
 
 // GESTION DES ROUTES PRINCIPALES
@@ -18,5 +20,11 @@ module.exports = app => {
 
     // DEVIS
     app.use(CONFIG.uri_prefix_main, devis);
+
+    //NEWS
+    app.use(CONFIG.uri_prefix_main, news);
+
+    //LIVRAISON
+    app.use(CONFIG.uri_prefix_main, livraison);
 
 };
