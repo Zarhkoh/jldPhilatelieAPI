db484ee9af5288279311770d34336099e755055e
Zarhkoh <zarhkoh@hotmail.fr>
2020-11-13 16:23:53 +0100

    modification de la bdd même en prod

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const db_init = require('./init_db/init_db');
 
 app.use(express.json());
 app.use(express.urlencoded({
-  limit: '10mb',
-  extended: true
+    limit: '10mb',
+    extended: true
 }));
 app.use(cors());
 
@@ -19,31 +19,30 @@ require('./src/modules/router.modules')(app);
 
 // redirect to /api-docs Swagger
 app.get('/', (req, res) =>
-  res.redirect('api-docs')
+    res.redirect('api-docs')
 );
 
 // Log Env
 console.log(`Environment: ${CONFIG.app}`);
 
 //Database init
-if (CONFIG.app === 'local') {
-  //Create database manually if it doesn't already exists because Sequelize don't handle it.
-  db_init.createDbIfNotExists();
-  // Sync Database
-  db.sequelize.sync({
+
+//Create database manually if it doesn't already exists because Sequelize don't handle it.
+db_init.createDbIfNotExists();
+// Sync Database
+db.sequelize.sync({
     // force: true pour forcer les changements sur la bdd
     force: false
-  }).then(function () {
+}).then(function() {
     console.log('La synchronisation avec la base de données a été effectuée avec succès');
-  }).catch(function (err) {
+}).catch(function(err) {
     console.log(err.message);
-  });
-}
+});
 
 app.listen(CONFIG.port, () => {
-  if (CONFIG.app === 'local') {
-    console.log(`Le serveur JLDPhilatelie est démarré sur le port ${CONFIG.port}`);
-  }
+    if (CONFIG.app === 'local') {
+        console.log(`Le serveur JLDPhilatelie est démarré sur le port ${CONFIG.port}`);
+    }
 });
 
 module.exports = app;
