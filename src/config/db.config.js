625f26766e5e89090c066ef3f602c9a14fcf49e6
Zarhkoh <zarhkoh@hotmail.fr>
2020-11-13 16:08:02 +0100

    ajout News/Livraison dans la création de bdd

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,18 +3,24 @@ const Timbre = require('../models').timbre;
 const User = require('../models').user;
 const Visitor = require('../models').visitor;
 const Devis = require('../models').devis;
+const Livraison = require('../models').livraison;
+const News = require('../models').news;
+
+
 
 
 const models = {
-  Timbre,
-  User,
-  Visitor,
-  Devis
+    Timbre,
+    User,
+    Visitor,
+    Devis,
+    Livraison,
+    News
 };
 
 const db = {
-  models,
-  sequelize
+    models,
+    sequelize
 };
 
 module.exports = db;
