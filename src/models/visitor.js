72df52212f1289633820218f9fce36d5deb248cf
Zarhkoh <zarhkoh@hotmail.fr>
2020-12-09 12:27:45 +0100

    changed DATEONLY to DATE to get timestamp too

--- src/models/visitor.js
+++ src/models/visitor.js
@@ -1,33 +1,33 @@
 module.exports = (sequelizeModels, Sequelize) => {
     const visitor = sequelizeModels.define(
         'visitor', {
-        visitorId: {
-            type: Sequelize.INTEGER(),
-            allowNull: false,
-            unique: true,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        visitorIp: {
-            type: Sequelize.STRING(50),
-            allowNull: false
-        },
-        visitorLastVisit: {
-            type: Sequelize.DATEONLY(),
-            allowNull: false
-        },
-        visitorBrowser: {
-            type: Sequelize.STRING(255)
-        },
-        visitCount: {
-            type: Sequelize.INTEGER(),
-            allowNull: false
+            visitorId: {
+                type: Sequelize.INTEGER(),
+                allowNull: false,
+                unique: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            visitorIp: {
+                type: Sequelize.STRING(50),
+                allowNull: false
+            },
+            visitorLastVisit: {
+                type: Sequelize.DATE(),
+                allowNull: false
+            },
+            visitorBrowser: {
+                type: Sequelize.STRING(255)
+            },
+            visitCount: {
+                type: Sequelize.INTEGER(),
+                allowNull: false
+            }
+        }, {
+            tableName: 'visitor',
+            timestamps: false,
+            freezeTableName: true,
         }
-    }, {
-        tableName: 'visitor',
-        timestamps: false,
-        freezeTableName: true,
-    }
     );
     return visitor;
 };
